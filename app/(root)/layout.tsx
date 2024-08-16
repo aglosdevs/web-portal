40a67a3aea5f04258c48c1333524b594b54ba3d1
kebzin <honorablewaiga@gmail.com>
2024-08-16 17:03:28 +0000

    created the root folder and auth

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { ThemeProvider } from "@/components/provider/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
