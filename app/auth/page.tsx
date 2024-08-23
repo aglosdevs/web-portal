2f99012e885041bbc1d490ba9454adc0c97dd2e0
kebzin <honorablewaiga@gmail.com>
2024-08-23 18:48:54 +0000

    auth layout

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -6,39 +6,105 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import Link from "next/link"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import * as z from "zod";
+import { useForm } from "react-hook-form/dist";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-const Auth = () =>{
-    return (
-      <section className="w-full max-w-sm h-[100vh] flex justify-center items-center m-auto">            
-        <Card>
-        <CardHeader>
-            <CardTitle className="text-2xl">Login</CardTitle>
-            <CardDescription>
-            Enter your email below to login to your account.
-            </CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-            <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="m@example.com" required />
-            </div>
-            <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
-            </div>
-        </CardContent>
-        <CardFooter className="flex-col gap-3">
-                    <Button className="w-full">Sign in</Button>
-                    <Link href="/auth/forgotPassword" className="text-muted-foreground block self-start">Forgot password</Link>
-        </CardFooter>
-        </Card>
-      </section>
-  )
-}
- 
+const Auth = () => {
+  const formSchema = z.object({
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(5, { message: "password must be atlist 5 character" })
+      .max(50),
+  });
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit() {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    // console.log(values);
+  }
+
+  return (
+    <section className="w-full max-w-sm h-[100vh] flex justify-center items-center m-auto">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-2xl">Login</CardTitle>
+              <CardDescription>
+                Enter your email below to login to your account.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <div className="grid gap-2">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter Email" {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="grid gap-2">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter Password" {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </CardContent>
+            <CardFooter className="flex-col gap-3">
+              <Button className="w-full" type="submit">
+                Sign in
+              </Button>
+              <Link
+                href="/auth/forgotPassword"
+                className="text-muted-foreground block self-start"
+              >
+                Forgot password
+              </Link>
+            </CardFooter>
+          </Card>
+        </form>
+      </Form>
+    </section>
+  );
+};
+
 export default Auth;
-
